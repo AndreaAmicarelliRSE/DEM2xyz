681e85eaad3d33009589ce7425e49def57e9e869
AndreaAmicarelliRSE <andrea.amicarelli@rse-web.it>
2016-10-25 17:11:08 +0200

    Minor modifications.    The output "xyz" file can be directly read by Paraview (the 4th column       replicates the coordinate z).    The number of output vertices is written in the application log.

--- src/Main/DEM2xyz.f90
+++ src/Main/DEM2xyz.f90
@@ -27,10 +27,10 @@ PROGRAM DEM2xyz
 ! Declarations
 !------------------------
 implicit none
-double precision,dimension(:,:),allocatable :: mat_z
-double precision :: dx   
 integer :: i,j,n_col,n_raw,res_fact,n_points
+double precision :: dx
 character(len=100) :: char_aux
+double precision,dimension(:,:),allocatable :: mat_z
 !------------------------
 ! Explicit interfaces
 !------------------------
@@ -44,46 +44,47 @@ character(len=100) :: char_aux
 ! Statements
 !------------------------
 ! Subroutine info
-print *,"!!!DEM2xyz is running:"
-print *,"This subroutine reads a DEM file"
-print *,"    and writes a corresponding xyz file." 
-print *,"Algorithm:"
-print *,"   1) Reading DEM file "
-print *,"   2) Writing xyz file "
+write(*,*) "***DEM2xyz is running:"
+write(*,*) "This subroutine reads a DEM file"
+write(*,*) "    and writes a corresponding xyz file." 
+write(*,*) "Algorithm:"
+write(*,*) "   1) Reading DEM file "
+write(*,*) "   2) Writing xyz file "
 ! 1) Reading DEM (start)
-print *,"1)  Reading DEM file "
-open (1,file='DEM.txt') 
-read (1,'(a14,i15)') char_aux,n_col
-read (1,'(a14,i15)') char_aux,n_raw
-read (1,'(a)')
-read (1,'(a)')
-read (1,'(a14,f15.7,i15)') char_aux,dx,res_fact
-read (1,'(a)')
+write(*,*) "1)  Reading DEM file "
+open(1,file='DEM.txt') 
+read(1,'(a14,i15)') char_aux,n_col
+read(1,'(a14,i15)') char_aux,n_raw
+read(1,'(a)')
+read(1,'(a)')
+read(1,'(a14,f15.7,i15)') char_aux,dx,res_fact
+read(1,'(a)')
 allocate(mat_z(n_raw,n_col))
 mat_z = 0.d0
 do i=1,n_raw
-   read (1,*) mat_z(i,:)
+   read(1,*) mat_z(i,:)
 enddo
 close(1)
-print *,"End Reading DEM file "
+write(*,*) "End Reading DEM file "
 ! End 1) Reading DEM file (end)
 ! 2) Writing xyz file (start)
-print *,"2)  Writing xyz file "
-open (2,file="xyz.txt")
-write (2,'(a)') '        X(m)       Y(m)        z(m)'
+write(*,*) "2)  Writing xyz file "
+open(2,file="xyz.txt")
+write(2,'(a)') '        x(m)       y(m)        z(m)        z(m)'
 n_points = n_raw*n_col/res_fact/res_fact
-write (2,'(i12)') n_points
+write(*,'(a,i15)') 'Number of vertices in the output "xyz" file: ',n_points
 do i=1,n_raw,res_fact
    do j=1,n_col,res_fact
-      write (2,'(3(F12.4))') (j-1)*dx+dx/2,(n_raw+1-i)*dx-dx/2,mat_z(i,j) 
-   end do   
-end do
-close (2)
-print *,"End Writing xyz file "
+      write(2,'(4(F12.4))') (j-1)*dx+dx/2,(n_raw+1-i)*dx-dx/2,mat_z(i,j),      &
+         mat_z(i,j) 
+   enddo   
+enddo
+close(2)
+write(*,*) "End Writing xyz file "
 ! End 2) Writing xyz file (end)
 !------------------------
 ! Deallocations
 !------------------------
 deallocate(mat_z)
-print *,"!!!  DEM2xyz has terminated "
+write(*,*) "***  DEM2xyz has terminated "
 end program DEM2xyz
