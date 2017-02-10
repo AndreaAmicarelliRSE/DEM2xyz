34c5c3f5e8aa913b77500ce1b85bf54f22596cf8
AndreaAmicarelliRSE <andrea.amicarelli@rse-web.it>
2017-02-10 20:19:16 +0100

    Minor modifications.    In case the absolute value of the mean latitude (abs_mean_latitude) is       provided with a non-negative value, the following conversion takes place:       (lon,lat) in (°) to (X,Y) in (m). In this case, an interpolation       (weighted on the square of the distance) is carried out to provide a       uniform Cartesian output grid in (X,Y).

--- src/Main/DEM2xyz.f90
+++ src/Main/DEM2xyz.f90
@@ -17,7 +17,12 @@
 !-------------------------------------------------------------------------------
 ! Description. “DEM2xyz v.1.0” (RSE SpA) reads a “DEM” file and writes the 
 !              associated DEM (or DTM) in a corresponding “xyz” file, possibly 
-!              changing the spatial resolution (as requested by the user).
+!              changing the spatial resolution (as requested by the user). In 
+!              case the absolute value of the mean latitude is provided with
+!              a non-negative value, the following conversion takes place 
+!              "(lon,lat) in (°) to (X,Y) in (m)". In this case, an 
+!              interpolation (weighted on the square of the distance) is 
+!              carried out to provide a uniform Cartesian output grid in (X,Y).
 !-------------------------------------------------------------------------------
 PROGRAM DEM2xyz
 !------------------------
@@ -27,10 +32,11 @@ PROGRAM DEM2xyz
 ! Declarations
 !------------------------
 implicit none
-integer :: i,j,n_col,n_raw,res_fact,n_points
-double precision :: dx
+integer :: i_in,j_in,i_out,j_out,n_col_in,n_col_out,n_raw,res_fact,n_points_in
+integer :: n_points_out
+double precision :: dx,dy,abs_mean_latitude,denom,distance,x_in,x_out,y_in,y_out
 character(len=100) :: char_aux
-double precision,dimension(:,:),allocatable :: mat_z
+double precision,dimension(:,:),allocatable :: mat_z_in,mat_z_out
 !------------------------
 ! Explicit interfaces
 !------------------------
@@ -48,43 +54,84 @@ write(*,*) "***DEM2xyz is running:"
 write(*,*) "This subroutine reads a DEM file"
 write(*,*) "    and writes a corresponding xyz file." 
 write(*,*) "Algorithm:"
-write(*,*) "   1) Reading DEM file "
-write(*,*) "   2) Writing xyz file "
-! 1) Reading DEM (start)
-write(*,*) "1)  Reading DEM file "
+write(*,*) "   1) Reading DEM file and pre-processing "
+write(*,*) "   2) Eventual grid interpolation and writing xyz file "
+write(*,*) "1)  Reading DEM file and pre-processing "
 open(1,file='DEM.txt') 
-read(1,'(a14,i15)') char_aux,n_col
+read(1,'(a14,i15)') char_aux,n_col_in
 read(1,'(a14,i15)') char_aux,n_raw
 read(1,'(a)')
 read(1,'(a)')
-read(1,'(a14,f15.7,i15)') char_aux,dx,res_fact
+read(1,'(a14,f15.7,i15,f15.7)') char_aux,dy,res_fact,abs_mean_latitude
+if (abs_mean_latitude>=0.d0) then
+abs_mean_latitude = abs_mean_latitude / 180.d0 * 3.1415926
+! Conversion (lon,lat) in (°) to (X,Y) in (m) for dx and dy
+! Linear unit discretization along the same parallel/latitude due to changing 
+! in longitude according to the DEM input discretization 
+   dx = dy * (111412.84d0 * dcos(abs_mean_latitude) - 93.5d0 * dcos(3.d0 *     &
+        abs_mean_latitude) + 0.118d0 * dcos(5.d0 * abs_mean_latitude))
+! Linear unit discretization along the same meridian/longitude due to changing 
+! in latitude according to the DEM input discretization 
+   dy = dy * (111132.92d0 - 559.82d0 * dcos(2.d0 * abs_mean_latitude) +        &
+        1.175d0 * dcos(4.d0 * abs_mean_latitude) - 0.0023d0 * dcos(6.d0 *      &
+        abs_mean_latitude))
+   n_col_out = floor(n_col_in * dx / dy)
+   else
+      dx = dy
+      n_col_out = n_col_in
+endif
 read(1,'(a)')
-allocate(mat_z(n_raw,n_col))
-mat_z = 0.d0
-do i=1,n_raw
-   read(1,*) mat_z(i,:)
+allocate(mat_z_in(n_raw,n_col_in))
+allocate(mat_z_out(n_raw,n_col_out))
+mat_z_in = 0.d0
+mat_z_out = 0.d0
+do i_in=1,n_raw
+   read(1,*) mat_z_in(i_in,:)
 enddo
 close(1)
-write(*,*) "End Reading DEM file "
-! End 1) Reading DEM file (end)
-! 2) Writing xyz file (start)
-write(*,*) "2)  Writing xyz file "
+write(*,*) "End Reading DEM file and pre-processing "
+write(*,*) "2)  Eventual grid interpolation and writing xyz file "
 open(2,file="xyz.txt")
 write(2,'(a)') '        x(m)       y(m)        z(m)        z   '
-n_points = n_raw*n_col/res_fact/res_fact
-write(*,'(a,i15)') 'Number of vertices in the output "xyz" file: ',n_points
-do i=1,n_raw,res_fact
-   do j=1,n_col,res_fact
-      write(2,'(4(F12.4))') (j-1)*dx+dx/2,(n_raw+1-i)*dx-dx/2,mat_z(i,j),      &
-         mat_z(i,j) 
-   enddo   
+n_points_in = n_raw * n_col_in / res_fact / res_fact
+n_points_out = n_raw * n_col_out / res_fact / res_fact
+write(*,'(a,i15)') 'Number of vertices in the input "DEM" file: ',n_points_in
+write(*,'(a,i15)') 'Number of vertices in the output "xyz" file: ',n_points_out
+do j_out=1,n_col_out,res_fact
+   do i_out=1,n_raw,res_fact
+      x_out = (j_out - 1) * dy + dy / 2.d0
+      y_out = (n_raw + 1 - i_out) * dy - dy / 2.d0
+      if (abs_mean_latitude>=0.d0) then      
+! Interpolation: inverse of the distance**2
+         denom = 0.d0
+         do j_in=1,n_col_in
+            do i_in=1,n_raw
+               x_in = (j_in - 1) * dx + dx / 2.d0
+               y_in = (n_raw + 1 - i_in) * dy - dy / 2.d0
+               distance = dsqrt((x_in - x_out) ** 2 + (y_in - y_out) ** 2)
+               if (distance<=(dx*10.d0)) then
+                  mat_z_out(i_out,j_out) = mat_z_out(i_out,j_out) +            &
+                                           mat_z_in(i_in,j_in) / distance ** 2
+                  denom = denom + 1.d0 / distance ** 2
+               endif
+            enddo
+         enddo
+         if (denom/=0.d0) mat_z_out(i_out,j_out) = mat_z_out(i_out,j_out) /    &
+                                                   denom
+         else
+! No interpolation (dx=dy)
+            mat_z_out(i_out,j_out) = mat_z_in(i_out,j_out)
+      endif
+      write(2,'(4(F12.4))') x_out,y_out,mat_z_out(i_out,j_out),                &
+         mat_z_out(i_out,j_out)
+   enddo
 enddo
 close(2)
-write(*,*) "End Writing xyz file "
-! End 2) Writing xyz file (end)
+write(*,*) "End Eventual grid interpolation and writing xyz file "
 !------------------------
 ! Deallocations
 !------------------------
-deallocate(mat_z)
+deallocate(mat_z_in)
+deallocate(mat_z_out)
 write(*,*) "***  DEM2xyz has terminated "
 end program DEM2xyz
