2136de0e503f471929f3ced66bbf1f4e218456ca
AndreaAmicarelliRSE <andrea.amicarelli@rse-web.it>
2017-11-02 10:43:31 +0100

    Minor modifications.

--- src/Geometry/line_line_intersection_2D.f90
+++ src/Geometry/line_line_intersection_2D.f90
@@ -1,6 +1,6 @@
 !-------------------------------------------------------------------------------
 ! "DEM2xyz v.1.0" (DEM manager tool)
-! Copyright 2016 (RSE SpA)
+! Copyright 2016-2017 (RSE SpA)
 ! "DEM2xyz v.1.0" authors and email contact are provided on the documentation 
 ! file.
 ! This file is part of DEM2xyz v.1.0 .
